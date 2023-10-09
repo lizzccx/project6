080490c133841616418aa9a3aeeb5abeac33fb9c
Ліза Ільїна <lizailina@Lizas-MacBook-Air.local>
2023-10-09 13:22:50 +0300

    add file

---
+++ index.swift
@@ -0,0 +1,30 @@
+present(alert, animated: true, completion: nil)
+    }
+    
+    func loginSuccess() {
+        updateUser()
+    }
+}
+
+// MARK: - Login flow functions -
+
+extension LoginViewController {
+    func credentialsExist() -> Bool {
+        return LoginCredentials.getCredentials() != nil
+    }
+    
+    func credentialsExpired() -> Bool {
+        if let credentials = LoginCredentials.getCredentials() {
+            return credentials.isExpired()
+        }
+        return true
+    }
+    
+    func updateUser() {
+        if APIManager.manager.reachability.connection == .none {
+            return
+        }
+        updateUserInProgress = true
+        interactor?.checkCurrentUserRequest()
+    }
+}
